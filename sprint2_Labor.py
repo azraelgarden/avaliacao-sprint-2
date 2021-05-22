64ddb687a1b47fdd1a00aa29ba77a7fec8707d90
GustavoMarcello <61936009+GustavoMarcello@users.noreply.github.com>
2021-05-22 12:42:44 -0300

    treinamento realizado
    Falta integração com o Mongo, possível curva de decisão e ajustes de alguns avisos na fase de 'ajustes de dados'

--- sprint2_Labor.py
+++ sprint2_Labor.py
@@ -1,10 +1,12 @@
-#Dataset utilizado: Labor
-#Esse Dataset foi usado para aprender as descrições de contratos aceitos e não aceitos
+# Dataset utilizado: Labor
+# Esse Dataset foi usado para aprender as descrições de contratos aceitos e não aceitos
 
-#*******descobrir como testar e treinar o código, a partir das aulas da alura*********
+# *******descobrir como testar e treinar o código, a partir das aulas da alura*********
 
+# ignorar a quantidade de bibliotecas por hora, depois selecionarei as utilizadas
 from re import X
 from typing import NamedTuple
+import numpy as np
 from numpy import NaN
 from pandas.core.algorithms import mode
 import pymongo
@@ -19,68 +21,68 @@ import openml
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-#conectando ao servidor/conectando ao banco/criando coleção
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# conectando ao servidor/conectando ao banco/criando coleção
 # myClient = pymongo.MongoClient("mongodb://localhost:27017/")
 # mydb = myClient["labor"]
 # myCol = mydb["contratos"]
 
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-#buscando dataset: Labor https://www.openml.org/d/4
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# buscando dataset: Labor https://www.openml.org/d/4
 dataset = openml.datasets.get_dataset(4)
-#printando informações do dataset
-#print(dataset)
+# printando informações do dataset
+# print(dataset)
 
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-#adicionando as informações do dataset a variável info
-x, y, categorical_indicator, attribute_names = dataset.get_data(dataset_format="dataframe", target=dataset.default_target_attribute)
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# adicionando as informações do dataset a variável info
+x, y, categorical_indicator, attribute_names = dataset.get_data(
+    dataset_format="dataframe", target=dataset.default_target_attribute)
 
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-#ajustando dados
-#alterando tipo de y: Series para DataFrame
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# ajustando dados
+# alterando tipo de y: Series para DataFrame
 y_DF = pd.DataFrame(y)
-#criando dados somente com as colunas desejadas
+# criando dados somente com as colunas desejadas
 dados = x[["duration", "standby-pay"]]
-#unindo y_DF com dados
+# unindo y_DF com dados
 dados['class'] = y_DF
-#excluindo "standby-pay" = NaN
+# excluindo "standby-pay" = NaN
 dados = dados.dropna()
-#print(dados)
+# print(dados)
 
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-#gera o gráfico
-sns.scatterplot(x=dados["duration"], y=dados["standby-pay"], hue=dados["class"], data=dados)
-#plt.show()
+# # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# gera o gráfico
+sns.scatterplot(x="duration", y="standby-pay", hue="class", data=dados)
+plt.show()
 
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-#treinos e testes
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# treinos e testes
 
-#separando dados x e y
+# separando dados x e y
 x = dados[["duration", "standby-pay"]]
-#print(dados_x)
+# print(dados_x)
 y = dados["class"]
-y = pd.DataFrame(y) #alterando tipo de dado de Series para DataFrame
-#print(dados_y)
+y = pd.DataFrame(y)  # alterando tipo de dado de Series para DataFrame
+# print(dados_y)
 
 # #verificando tipos de dados raw_dados_x/raw_dados_y
-# print(type(dados_x))
+# print(type(x))
 # print('')
-# print(type(dados_y))
+# print(type(y))
 
-# #qtde de treino (total 9)
-treino_x = x[:6]
-treino_y = y[:6]
-#qtde de teste (total 9)
-teste_y = x[6:]
-teste_x = y[6:]
-print(f'Treinaremos com {len(treino_x)} e testaremos com {len(teste_x)} elementos')
+#definindo semente fixa para train_test_split e LinearSVC
+SEED = 2
+np.random.seed(SEED)
+
+treino_x, teste_x, treino_y, teste_y = train_test_split(
+    x, y, test_size=0.3, stratify=y)
+print(
+    f'Treinaremos com {len(treino_x)} e testaremos com {len(teste_x)} elementos')
 
 # #rodando e treinando o modelo
 modelo = LinearSVC()
-modelo.fit(treino_x, treino_y)
+modelo.fit(treino_x, treino_y.values.ravel())  # convertendo para uma dimensão
 previsoes = modelo.predict(teste_x)
 acuracia = accuracy_score(teste_y, previsoes) * 100
-print(f'A acurácia foi de {acuracia:.2f}')
-
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
+print(f'A acurácia foi de {acuracia:.2f} %')
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
