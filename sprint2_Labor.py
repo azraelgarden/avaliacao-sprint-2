a863664bc8b6481d73c8789e0cd65275c1d67630
GustavoMarcello <61936009+GustavoMarcello@users.noreply.github.com>
2021-05-23 23:23:09 -0300

    subindo dados ao mongoDB
    ;;

--- sprint2_Labor.py
+++ sprint2_Labor.py
@@ -1,6 +1,6 @@
 # Dataset utilizado: Labor. Esse Dataset foi usado para aprender as descrições de contratos aceitos e não aceitos
 # Este programa acessa o DataSet, faz a tratativa dos dados, selecionando 2 de seus treasures,
-#plota um grafico com os dados já tratados, depois treina esses dados e armazena os resultados no MongoDB
+# plota um grafico com os dados já tratados, depois treina esses dados e armazena os resultados no MongoDB
 
 import numpy as np
 import pymongo
@@ -47,6 +47,7 @@ plt.show()
 x = dados[["duration", "standby-pay"]]
 # print(dados_x)
 y = dados["class"]
+y = pd.DataFrame(y)
 # print(dados_y)
 
 # #verificando tipos de dados raw_dados_x/raw_dados_y
@@ -54,14 +55,15 @@ y = dados["class"]
 # print('')
 # print(type(y))
 
-#definindo semente fixa para train_test_split e LinearSVC
+# definindo semente fixa para train_test_split e LinearSVC
 SEED = 5
 np.random.seed(SEED)
 
-#algorítimo de treino
+# algorítimo de treino
 treino_x, teste_x, treino_y, teste_y = train_test_split(
     x, y, test_size=0.3, stratify=y)
-print(f'Treinaremos com {len(treino_x)} e testaremos com {len(teste_x)} elementos')
+print(
+    f'Treinaremos com {len(treino_x)} e testaremos com {len(teste_x)} elementos')
 
 # #rodando e treinando o modelo
 modelo = LinearSVC()
@@ -72,17 +74,24 @@ print(f'A acurácia foi de {acuracia:.2f} %')
 
 
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-#inserindo valores no banco
+# inserindo valores no banco
 
-#conectando ao servidor/conectando ao banco/criando coleção
+# conectando ao servidor/conectando ao banco/criando coleção
 myClient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myClient["labor"]
 mycol = mydb["contratos"]
 
-dic = {"treinados": len(treino_x),"testados": len(teste_x), "acuracia": acuracia}
+#inserindo dados no mongodb
+dados.reset_index(inplace=True)
+dados = dados.to_dict('records')
+mycol.insert_many(dados)
+
+#inserindo qtde de treinados/qtde de testados/acuracia no mongodb
+dic = {"treinados": len(treino_x), "testados": len(
+    teste_x), "acuracia": acuracia}
 mycol.insert_one(dic)
 
-#visualizando os valores
+# visualizando os valores
 for x in mycol.find():
     print(x)
 
